cec5287f1069207205d86f4712b4b71d22a1a400
“Damian <damiang58@gmail.com>
2018-06-12 13:07:07 -0400

    v3

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var apiURL = 'http://voteapi.empower2018.us';
 $(document).ready(function () {
 	$('#getResults').click(function() {
 		$.get(apiURL + '/tally', function(data) {
-			var tallyResults = "<thead><tr><th scope='col'>API</th><th scope='col'>Vote Count</th><th scope='col'>API ID</th></tr></thead>";
+			var tallyResults = "<thead class='thead-dark'><tr><th scope='col'>API</th><th scope='col'>Vote Count</th><th scope='col'>API ID</th></tr></thead>";
 			for (var i = 0; i < data.length; i++) {
 				tallyResults += "<tr><td>" + data[i].api_name + "</td><td>" + data[i].vote_count + "</td><td>" + data[i].api_id + "</td></tr>";
 			}
@@ -12,9 +12,9 @@ $(document).ready(function () {
 	});
 	$('#getOptions').click(function() {
 		$.get(apiURL + '/apis', function(data) {
-			var optionsResults = "<thead><tr><th scope='col'>API</th><th scope='col'>API ID</th></tr></thead>";
+			var optionsResults = "<thead class='thead-dark'><tr><th scope='col'>API</th><th scope='col'>API ID</th></tr></thead>";
 			for (var i = 0; i < data.length; i++) {
-				optionsResults += "<tr><td><button class='api'>" + data[i].name + "</td></button><td>" + "   " + data[i]._id + "</td></tr>";
+				optionsResults += "<tr><td><button class='api btn btn-dark'>" + data[i].name + "</td></button><td>" + "   " + data[i]._id + "</td></tr>";
 				var name = data[i].api_name;
 				$(document).on("click", ".api" , function (data) {
 					console.log("Got here");
